1ac568a521fc793a622ab38ff19f348b57c56861
Francisco Heraclito Pantoja da Silva Junior <francisco.junior@fetz.interno.local>
2025-11-27 09:30:36 -0300

    configuração do sistema

---
+++ config/routes.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+  'home'         => 'pages/home.php',
+
+  'rh/rh-ponto'     => 'pages/rh/rh-ponto.php',
+  'rh/rh-folha'     => 'pages/rh/rh-folha.php',
+  'rh/rh-pessoas'   => 'pages/rh/rh-pessoas.php',
+  'rh/rh-sso'       => 'pages/rh/rh-sso.php',
+  'rh/rh-ponto-permissoes' => 'pages/rh/rh-ponto-permissoes.php',
+
+  'bo/bo-contabil'    => 'pages/bo/bo-contabil.php',
+  'bo/bo-estoque'     => 'pages/bo/bo-estoque.php',
+  'bo/bo-financeira'  => 'pages/bo/bo-financeira.php',
+  'bo/bo-fiscal'      => 'pages/bo/bo-fiscal.php',
+  'bo/bo-patrimonial' => 'pages/bo/bo-patrimonial.php',
+];
